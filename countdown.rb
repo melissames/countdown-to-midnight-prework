830bbd3c4b794035e4d276acac0a035f5aacb80d
melissames <melissa.sames@gmail.com>
2018-01-08 22:12:19 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(new_years)
+  counter = 0
+
+  while counter < new_years
+    puts "#{counter} SECOND(S)!"
+    counter += 1
+  end
+  "HAPPY NEW YEAR!"  
 end
