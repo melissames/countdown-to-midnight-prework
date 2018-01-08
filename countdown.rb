ae63b99c9f9f52c8194934f2218bc0808151006f
melissames <melissa.sames@gmail.com>
2018-01-08 22:15:30 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,6 +6,16 @@ def countdown(new_years)
   while counter > 0
     puts "#{counter} SECOND(S)!"
     counter -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(new_years)
+  counter = new_years
+
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
     sleep(1)
   end
   "HAPPY NEW YEAR!"
